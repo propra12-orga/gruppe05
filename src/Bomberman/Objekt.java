58977a6e3aff67b6ace60777fe4bdb35b177b355
Mike Bechtel <Mike.Bechtel@uni-duesseldorf.de>
2012-05-18 03:10:25 +0200

    README hinzugefuegt.

--- src/Bomberman/Objekt.java
+++ src/Bomberman/Objekt.java
@@ -100,87 +100,15 @@ public abstract class Objekt extends Rectangle2D.Double implements Drawable, Mov
 
 	public abstract boolean collidedWith(Objekt s);
 	
-	/*public boolean checkOpaqueColorCollisions(Objekt s)
-	{   
-    Rectangle2D.Double cut = (Double) this.createIntersection(s);
-    
-    if((cut.width<1)||(cut.height<1)){
-      return false;
-    }
-    
-    // Rechtecke in Bezug auf die jeweiligen Images
-    Rectangle2D.Double sub_me = getSubRec(this,cut);
-    Rectangle2D.Double sub_him = getSubRec(s,cut);
-    
-    BufferedImage img_me = pics[currentpic].getSubimage((int)sub_me.x,(int)sub_me.y,
-    													(int)sub_me.width,(int)sub_me.height);
-    BufferedImage img_him = s.pics[s.currentpic].getSubimage((int)sub_him.x,(int)sub_him.y,
-    													(int)sub_him.width,(int)sub_him.height);
-    
-    for(int i=0;i<img_me.getWidth();i++){
-      for(int n=0;n<img_him.getHeight();n++){
-
-        int rgb1 = img_me.getRGB(i,n); 
-        int rgb2 = img_him.getRGB(i,n);
-
-        
-        if(isOpaque(rgb1)&&isOpaque(rgb2)){
-          return true;
-        }
-        
-      }
-    }
-    
-    return false;
-  }
   
-  protected Rectangle2D.Double getSubRec(Rectangle2D.Double source, Rectangle2D.Double part) {
-    
-    //Rechtecke erzeugen
-    Rectangle2D.Double sub = new Rectangle2D.Double();
-    
-    //get X - compared to the Rectangle
-    if(source.x>part.x){
-      sub.x = 0;
-    }else{
-      sub.x = part.x - source.x;
-    }
-    
-    if(source.y>part.y){
-      sub.y = 0;
-    }else{
-      sub.y = part.y - source.y;
-    }
-
-    sub.width = part.width;
-    sub.height = part.height;
-    
-    return sub;
-  }
+	public int getLoopFrom()
+  	{
+	  	return loop_from;
+  	}
   
-  protected boolean isOpaque(int rgb) {
-
-    int alpha = (rgb >> 24) & 0xff;  
-    //red   = (rgb >> 16) & 0xff;  
-    //green = (rgb >>  8) & 0xff;  
-    //blue  = (rgb ) & 0xff;  
-    
-    if(alpha==0){
-      return false;
-    }
-
-    return true;
-    
-  }*/
-  
-  public int getLoopFrom()
-  {
-	  return loop_from;
-  }
-  
-  public int getLoopTo()
-  {
-	  return loop_to;
-  }
+  	public int getLoopTo()
+  	{
+	  	return loop_to;
+  	}
   
 }
